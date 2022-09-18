2f5f9cc896311879b850e0f45986e6b553c49b8c
David Rasch <dcrasch@gmail.com>
2022-09-18 11:15:35 +0200

    remove weealloc

--- docs/index-f0c5c2366e77ec41.js
+++ docs/index-f0c5c2366e77ec41.js
@@ -201,7 +201,7 @@ function makeMutClosure(arg0, arg1, dtor, f) {
     return real;
 }
 function __wbg_adapter_14(arg0, arg1, arg2) {
-    wasm._dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h43963f2011a285e3(arg0, arg1, addHeapObject(arg2));
+    wasm._dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h52731b523f000374(arg0, arg1, addHeapObject(arg2));
 }
 
 function isLikeNone(x) {
@@ -405,12 +405,12 @@ function getImports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper72 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 29, __wbg_adapter_14);
+    imports.wbg.__wbindgen_closure_wrapper62 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 23, __wbg_adapter_14);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper74 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 29, __wbg_adapter_14);
+    imports.wbg.__wbindgen_closure_wrapper64 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 23, __wbg_adapter_14);
         return addHeapObject(ret);
     };
 
@@ -445,7 +445,7 @@ function initSync(bytes) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-88b6e15083a857b2_bg.wasm', import.meta.url);
+        input = new URL('index-f0c5c2366e77ec41_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
