aaadf26b4531cc801da3dcdfb096601f7d85e358
David Rasch <david.rasch@cwi.nl>
2023-07-09 18:33:00 +0200

    add svg renderer

--- tessellations/src/render/mod.rs
+++ tessellations/src/render/mod.rs
@@ -1,8 +1,9 @@
 mod raqote_render;
+mod svg_render;
+
 pub use raqote_render::Backend;
 pub use raqote_render::OutputImage;
 pub use raqote_render::Render;
-mod svg_render;
-pub use svg_render::SVGBackend;
 pub use svg_render::Compose;
-
+pub use svg_render::SVGBackend;
+pub use svg_render::SVGImage;
--- tessellations/src/render/svg_render.rs
+++ tessellations/src/render/svg_render.rs
@@ -2,31 +2,31 @@ use crate::tessellationfigure::{TessellationFigure, TessellationPlane};
 use euclid::Angle;
 use raqote::*;
 
-use svg::Document;
+use svg::node::element::path::Command;
+use svg::node::element::path::Data;
+use svg::node::element::path::Position::Absolute;
 use svg::node::element::Path;
 use svg::node::element::SVG;
-use svg::node::element::path::Data;
-use svg::node::element::path::Command;
-use svg::node::element::path::Position::Absolute;
+use svg::Document;
+use svg::Node;
 
 #[derive(Clone, Copy)]
 pub struct SVGBackend;
 
 pub trait Compose {
     /// Compose a figure to a document
-    fn compose(
+    fn compose(&self, figure: &TessellationFigure, m: &Transform) -> Option<SVG>;
+
+    fn compose_plane(
         &self,
+        plane: &TessellationPlane,
         figure: &TessellationFigure,
         m: &Transform,
     ) -> Option<SVG>;
 }
 
 impl Compose for SVGBackend {
-    fn compose(
-        &self,
-        figure: &TessellationFigure,
-        m: &Transform,
-    ) -> Option<SVG> {
+    fn compose(&self, figure: &TessellationFigure, m: &Transform) -> Option<SVG> {
         let points = figure
             .points()
             .windows(2)
@@ -40,23 +40,103 @@ impl Compose for SVGBackend {
             .collect::<Vec<Point>>();
         let p1 = points[0];
 
-	    let mut pb = Data::new();
-	    pb.append(Command::Move(Absolute, (p1.x,p1.y).into()));
+        let mut pb = Data::new();
+        pb.append(Command::Move(Absolute, (p1.x, p1.y).into()));
 
-        for p in points.iter().skip(1) {    
-            pb.append(Command::Line(Absolute, (p.x,p.y).into()));
+        for p in points.iter().skip(1) {
+            pb.append(Command::Line(Absolute, (p.x, p.y).into()));
         }
         pb.append(Command::Close);
         let path = Path::new()
-             .set("fill", "none")
-             .set("stroke", "black")
-             .set("stroke-width", 1)
-             .set("d", pb);
-        
-         let document = Document::new()
-            .set("viewBox", (0, 0, 400, 400))
-             .add(path);
-        svg::save("image.svg", &document).unwrap();
+            .set("fill", "none")
+            .set("stroke", "black")
+            .set("stroke-width", 1)
+            .set("d", pb);
+
+        let document = Document::new().set("viewBox", (0, 0, 400, 400)).add(path);
         return Some(document);
     }
+
+    fn compose_plane(
+        &self,
+        plane: &TessellationPlane,
+        figure: &TessellationFigure,
+        m: &Transform,
+    ) -> Option<SVG> {
+        let mut row = 0;
+        let g = plane.grid(figure, 400.0, 400.0, 70.);
+        let mut c = 0;
+        let mut document = Document::new().set("viewBox", (0, 0, 400, 400));
+        let colors = vec!["red", "green", "blue", "black"];
+
+        for rotdiv in 1..=figure.rotdiv {
+            let angle = Angle::degrees(360.0 * (rotdiv as f32) / (figure.rotdiv as f32));
+
+            for gridrow in &g {
+                if !figure.is_reversed {
+                    c = row % 2; // use for brick
+                }
+                for gridpoint in gridrow {
+                    if figure.is_reversed {
+                        c = rotdiv - 1; // for diamond
+                    }
+                    if !figure.is_reversed && figure.gridincy < figure.gridincx {
+                        c = row % 3; // used for hexagon
+                    }
+
+                    let m = Transform::rotation(angle)
+                        .then_scale(70.0, 70.0)
+                        .then_translate(euclid::vec2(gridpoint.x, gridpoint.y));
+                    let points = figure
+                        .points()
+                        .windows(2)
+                        .filter_map(|l| {
+                            if l[0] != l[1] {
+                                Some(m.transform_point(l[0]))
+                            } else {
+                                None
+                            }
+                        })
+                        .collect::<Vec<Point>>();
+                    let p1 = points[0];
+
+                    let mut pb = Data::new();
+                    pb.append(Command::Move(Absolute, (p1.x, p1.y).into()));
+
+                    for p in points.iter().skip(1) {
+                        pb.append(Command::Line(Absolute, (p.x, p.y).into()));
+                    }
+                    pb.append(Command::Close);
+                    let color = colors[(c % 4) as usize];
+                    let path = Path::new()
+                        .set("fill", color)
+                        .set("stroke", "black")
+                        .set("stroke-width", 1)
+                        .set("d", pb);
+
+                    document.append(path);
+
+                    c += 1;
+                }
+                row += 1;
+            }
+        }
+        Some(document)
+    }
+}
+
+pub trait SVGImage {
+    fn save_svg(&self, path: &std::path::Path) -> bool;
+}
+
+impl SVGImage for Document {
+    fn save_svg(&self, path: &::std::path::Path) -> bool {
+        svg::save(path, self).is_ok()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
 }
